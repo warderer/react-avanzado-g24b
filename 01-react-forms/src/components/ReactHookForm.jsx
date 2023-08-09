ed27ff26d993c93b4695bc161ea0972a991ae9e3
César Guerra <warderer@gmail.com>
2023-08-08 21:37:42 -0600

    ✨ Validaciones con Yup

--- 01-react-forms/src/components/ReactHookForm.jsx
+++ 01-react-forms/src/components/ReactHookForm.jsx
@@ -1,9 +1,18 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import logo from '../assets/react.svg'
 
 const ReactHookForm = () => {
   // API: https://dummyjson.com/docs/users
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const schema = yup.object({
+    firstName: yup.string('El campo nombre debe ser un texto').required('El campo nombre es requerido'),
+    age: yup.number('El valor debe ser un número').positive('La edad debe ser un número positivo').integer().required('Debes especificar una edad')
+  }).required()
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   const onSubmit = data => console.log(data)
 
@@ -24,6 +33,7 @@ const ReactHookForm = () => {
             id='firstName'
             {...register('firstName', { required: true, maxLength: 20 })}
           />
+          <p>{errors.firstName?.message}</p>
 
           <label htmlFor='lastName'>Apellido</label>
           <input
@@ -42,6 +52,7 @@ const ReactHookForm = () => {
             id='age'
             {...register('age')}
           />
+          <p>{errors.age?.message}</p>
 
           <label htmlFor='genre'>Genero</label>
           <select name='genre' id='genre' {...register('genre')}>
