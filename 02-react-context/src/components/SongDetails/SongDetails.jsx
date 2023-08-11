df8ea43f5dc367cb5e656a3f941d28fae0ee752b
César Guerra <warderer@gmail.com>
2023-08-11 00:35:38 -0600

    🚨 Fix linter indent warning

--- 02-react-context/src/components/SongDetails/SongDetails.jsx
+++ 02-react-context/src/components/SongDetails/SongDetails.jsx
@@ -5,12 +5,14 @@ const SongDetails = () => {
   return (
     <>
       {selectedSong.title
-        ? <div>
-          <img src={selectedSong.img_url} alt={selectedSong.title} />
-          <h2>{selectedSong.title}</h2>
-          <p>{selectedSong.artist}</p>
-          <p>{selectedSong.year}</p>
-        </div>
+        ? (
+          <div>
+            <img src={selectedSong.img_url} alt={selectedSong.title} />
+            <h2>{selectedSong.title}</h2>
+            <p>{selectedSong.artist}</p>
+            <p>{selectedSong.year}</p>
+          </div>
+          )
         : <h1>Selecciona una canción</h1>}
     </>
   )
