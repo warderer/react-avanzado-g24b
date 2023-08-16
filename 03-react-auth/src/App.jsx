31ea2e2d4f91d943eb3512129255790dc0d0143a
César Guerra <warderer@gmail.com>
2023-08-15 20:55:49 -0600

    ✨ Estructura base del proyecto

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { Home, Dashboard, Login, Signup, Secret } from '@/pages'
+import Header from '@/components/Header/Header'
 
 function App () {
   return (
     <>
+      <Header />
       <h1>React App</h1>
+      <Home />
+      <Dashboard />
+      <Login />
+      <Signup />
+      <Secret />
     </>
   )
 }
---
+++ 03-react-auth/src/pages/index.js
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard'
+import Home from './Home'
+import Login from './Login'
+import Secret from './Secret'
+import Signup from './Signup'
+
+export {
+  Dashboard,
+  Home,
+  Login,
+  Secret,
+  Signup
+}
