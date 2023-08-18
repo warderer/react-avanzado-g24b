ac9e1f0c07d2dec92d469c58f1a1c6711cf9f064
Mackaber Witckin <mkbravo555@gmail.com>
2023-08-17 22:26:27 -0500

    Login implemented with axios

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,19 +1,49 @@
 import logo from '@/assets/react.svg'
 import '@/styles/form.css'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sendData = async (data) => {
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://ecommerce-json-jwt.onrender.com/login',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify(data)
+    }
+
+    axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    sendData({ email, password })
+  }
+
   return (
     <main className='form-signin w-100 m-auto'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <img className='mb-4' src={logo} alt='React' width='72' height='57' />
         <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
 
         <div className='form-floating'>
-          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com' />
+          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com' onChange={(event) => setEmail(event.target.value)} />
           <label htmlFor='floatingInput'>Email address</label>
         </div>
         <div className='form-floating'>
-          <input type='password' className='form-control' id='floatingPassword' placeholder='Password' />
+          <input type='password' className='form-control' id='floatingPassword' placeholder='Password' onChange={(event) => setPassword(event.target.value)} />
           <label htmlFor='floatingPassword'>Password</label>
         </div>
 
