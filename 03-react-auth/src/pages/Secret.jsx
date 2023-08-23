641ba28acd75eaf5b629eecc0b2f293f3e060460
Mackaber Witckin <mkbravo555@gmail.com>
2023-08-22 21:06:57 -0500

    WIP

--- 03-react-auth/src/pages/Secret.jsx
+++ 03-react-auth/src/pages/Secret.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react"
+import { getAllUsersService } from "../services/userService"
+
 const Secret = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsersData = async () => {
+      try {
+        const token = localStorage.getItem("jwt_token")
+        const response = await getAllUsersService(token)
+        setUsers(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchUsersData()
+  }, [])
+
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      <div>
+        {users.map(user => 
+          {<p></p>}
+          )}
+      </div>
+    </>
   )
 }
 export default Secret
--- 03-react-auth/src/services/userService.js
+++ 03-react-auth/src/services/userService.js
@@ -3,10 +3,12 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:3000'
 // const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
-const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const loginUserService = (data) => axios.get(`${BASE_URL}/login`, data)
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
+const getAllUsersService = (jwtToken) => axios.get(`${BASE_URL}/users`, { headers: { authorization: `Bearer ${jwtToken}` } })
 
 export {
   loginUserService,
-  registerUserService
+  registerUserService,
+  getAllUsersService
 }
